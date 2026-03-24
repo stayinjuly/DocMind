cf9d5afcdc7e0d567e3df33b6956e316e76db35a
Zemin Li <33084874+stayinjuly@users.noreply.github.com>
2026-03-24 18:31:31 +0800

    project init

---
+++ src/test/java/com/zm/docmind/DocMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zm.docmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocMindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
