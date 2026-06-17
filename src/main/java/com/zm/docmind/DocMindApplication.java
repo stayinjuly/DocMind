29f20c08855b7bb2e5e1000b5aca0203dbabd764
Zemin Li <stayinjuly@users.noreply.github.com>
2026-06-17 12:08:21 +0800

    chore: 移除冗余的@EnableScheduling

--- src/main/java/com/zm/docmind/DocMindApplication.java
+++ src/main/java/com/zm/docmind/DocMindApplication.java
@@ -3,12 +3,10 @@ package com.zm.docmind;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class DocMindApplication {
 
